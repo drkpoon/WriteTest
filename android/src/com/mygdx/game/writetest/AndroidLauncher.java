cda61c17f92df89b187bf6a6efba05585bd3f8f7
Derek Poon <drkpoon@hgcbroadband.com>
2018-07-12 23:01:14 +0800

    platform update

--- android/src/com/mygdx/game/writetest/AndroidLauncher.java
+++ android/src/com/mygdx/game/writetest/AndroidLauncher.java
@@ -3,32 +3,36 @@ package com.mygdx.game.writetest;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
 public class AndroidLauncher extends AndroidApplication {
-	public static final int REQUEST_CODE = 300;
+    public static final int REQUEST_CODE = 300;
     public static final String ANDROID_PERMISSION_WRITE_EXTERNAL_STORAGE = "android.permission.WRITE_EXTERNAL_STORAGE";
     private WriteTest writeTest;
 
     @Override
-	protected void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
         writeTest = new WriteTest();
         if (! checkWritePermission()){
             requestWritePermission();
         }
+//        System.out.println(isExternalStorageWritable());
         initialize(writeTest, config);
-	}
+    }
+
+    /* Checks if external storage is available for read and write */
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        return Environment.MEDIA_MOUNTED.equals(state);
+    }
 
     public boolean checkWritePermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            return(checkSelfPermission(ANDROID_PERMISSION_WRITE_EXTERNAL_STORAGE)==PackageManager.PERMISSION_GRANTED);
-        }else {
-            return true;
-        }
+        return Build.VERSION.SDK_INT < Build.VERSION_CODES.M || (checkSelfPermission(ANDROID_PERMISSION_WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED);
     }
 
     public void requestWritePermission() {
